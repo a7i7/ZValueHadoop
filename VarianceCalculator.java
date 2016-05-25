1f7c9436941cc151d7b43341c408bc4c72b67b9e
a7i7 <getafif22@gmail.com>
2016-05-25 12:06:58 +0530

    Variance

--- VarianceCalculator.java
+++ VarianceCalculator.java
@@ -7,19 +7,21 @@ import org.apache.hadoop.fs.Path;
 import org.apache.hadoop.conf.*;
 import java.math.*;
 
-public class AverageCalculator {
+public class VarianceCalculator {
 
-	private static final int TOTAL_COMBINERS = 3;
+	private static final int TOTAL_COMBINERS = 30;
 	private static int currentKey = 0;
+	public static Double mean = 3.5;
 	public static class Map extends MapReduceBase implements Mapper<LongWritable, Text, IntWritable, Text>
 	{
 		public void map(LongWritable key,Text value, OutputCollector<IntWritable,Text> output,Reporter reporter) throws IOException
 		{
 			DoubleWritable dataVal = new DoubleWritable(new Double(value.toString()));
 			int currentKey = (int)(Math.random()*TOTAL_COMBINERS);
-			output.collect(new IntWritable(currentKey),new Text(value.toString()+","+1));
-			// java.util.logging.Logger.getLogger(AverageCalculator.class.getName()).log(java.util.logging.Level.SEVERE,newKey+" "+value.toString(),"");
-
+			BigDecimal differenceFromMean = new BigDecimal(value.toString());
+			differenceFromMean = differenceFromMean.subtract(new BigDecimal(mean.toString()));
+			differenceFromMean = differenceFromMean.multiply(differenceFromMean);
+			output.collect(new IntWritable(currentKey),new Text(differenceFromMean.toString()+","+1));
 		}
 	}
 
@@ -27,7 +29,14 @@ public class AverageCalculator {
 	{
 		public void reduce(IntWritable key, Iterator<Text> values, OutputCollector<IntWritable,Text> output, Reporter reporter) throws IOException
 		{
-
+			if(!true)
+			{
+				String ans = "";
+				while(values.hasNext())
+					ans+=values.next()+",";
+				output.collect(key,new Text(ans.toString()));
+				return;
+			}
 			BigDecimal currentAverage = new BigDecimal("0.0");
 			BigDecimal currentCount = new BigDecimal("0.0");
 			while(values.hasNext())
@@ -55,8 +64,8 @@ public class AverageCalculator {
 
 	public static void main(String args[]) throws Exception
 	{
-		JobConf conf = new JobConf(AverageCalculator.class);
-		conf.setJobName("Average");
+		JobConf conf = new JobConf(VarianceCalculator.class);
+		conf.setJobName("Variance");
 
 		conf.setOutputKeyClass(IntWritable.class);
 		conf.setOutputValueClass(Text.class);
@@ -68,8 +77,8 @@ public class AverageCalculator {
 		conf.setInputFormat(TextInputFormat.class);
 		conf.setOutputFormat(TextOutputFormat.class);
 		
-		FileInputFormat.setInputPaths(conf,new Path("so_many_numbers.txt"));
-		FileOutputFormat.setOutputPath(conf,new Path("ans"));
+		FileInputFormat.setInputPaths(conf,new Path("so_many_numbers.txt.bak"));
+		FileOutputFormat.setOutputPath(conf,new Path("variance_ans"));
 		JobClient.runJob(conf);
 		return;
 	}
---
+++ random_number_generator.py
@@ -0,0 +1,5 @@
+import random
+random.seed(200)
+n = 20
+for i in range(n):
+	print str(random.randint(0,50))+".0"
